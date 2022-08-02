0e5199d2f5574123ce263469305619b31cb57763
Adam Firdaus D <adamfirdaus345@gmail.com>
2022-08-02 18:52:14 +0700

    Ver.1

---
+++ app/src/main/java/com/example/tubesakb/ui/profile/ProfileFragment.java
@@ -0,0 +1,43 @@
+package com.example.tubesakb.ui.profile;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import com.example.tubesakb.databinding.FragmentProfileBinding;
+
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+//        AboutViewModel dashboardViewModel =
+//                new ViewModelProvider(this).get(AboutViewModel.class);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+//        final TextView textView = binding.textDashboard;
+//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
