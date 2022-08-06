d7e488570f38fed10a24c933e732b77acdd1cda5
Adam Firdaus D <72844680+eDemson88@users.noreply.github.com>
2022-08-06 21:31:17 +0700

    Update AboutFragment.java

--- app/src/main/java/com/example/tubesakb/ui/about/AboutFragment.java
+++ app/src/main/java/com/example/tubesakb/ui/about/AboutFragment.java
@@ -1,4 +1,8 @@
 package com.example.tubesakb.ui.about;
+//Tanggal pengerjaan = 05-08-2022
+//NIM = 10119052, 10119062, 10119073, 10119078, 10119079
+//Nama = Rizki Dwi Nugraha, Naufal Rafi Pratama, Aghnia Dewi Mahiranie, Adam Firdaus Darmawan, Rafi Khalifanan Azanan
+//Kelas = IF-2
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
